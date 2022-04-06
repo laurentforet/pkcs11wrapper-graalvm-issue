d82843066d78b27fc4d2e048ef9089b21085cfff
Laurent Forêt <laurent.foret@devcoop.fr>
2022-04-06 16:20:47 +0200

    initial version

---
+++ Pkcs11WrapperLoader.java
@@ -0,0 +1,17 @@
+
+import java.io.File;
+
+public class Pkcs11WrapperLoader {
+
+    public static final String WRAPPER = "pkcs11wrapper-win-x86.dll";
+
+    public static void main(String[] args) {
+
+        final String wrapper = args.length == 0 ? WRAPPER : args[0];
+        System.out.println("WRAPPER = " + wrapper);
+        System.load(new File(wrapper).getAbsolutePath());
+        System.out.println("Loaded");
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+``` bash
+javac Pkcs11WrapperLoader.java
+native-image -cp . Pkcs11WrapperLoader
+pkcswrapperloader.exe
+```
